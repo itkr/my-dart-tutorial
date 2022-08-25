4dfe27f1e67d3efac3934f9eecf8edc33709ed4f
Shota Itakura <1531860+itkr@users.noreply.github.com>
2022-08-25 16:51:47 +0900

    Rename

--- hello.dart
+++
@@ -1,5 +0,0 @@
-import 'dart:core';
-
-void main(List<String> args) {
-  print('hello');
-}
---
+++ main.dart
@@ -0,0 +1,15 @@
+import 'dart:core';
+
+void calc() {
+  print(1 + 2 - 3 * 4 / 5 % 6);
+}
+
+void main(List<String> args) {
+  print('hello');
+  calc();
+
+  var a = 1;
+  print(a);
+  a = 2;
+  print('a = $a');
+}
